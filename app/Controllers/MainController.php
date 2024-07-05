f200bff69dbd899eb6b87b2111d4263ec95acb01
chipslays <hello@chipslays.ru>
2024-07-05 11:03:32 +0400

    fix(stats): increment

--- app/Controllers/MainController.php
+++ app/Controllers/MainController.php
@@ -12,14 +12,19 @@ class MainController
 {
     public function beforeRun(Context $context)
     {
-        if (!Session::has('already_call')) {
-            user()->increment('call_count');
+        if (!Session::get('already_call')) {
             Session::set('already_call', true);
+            user()->increment('call_count');
         }
     }
 
     public function start(Context $context)
     {
+        if (!Session::get('already_call')) {
+            Session::set('already_call', true);
+            user()->increment('call_count');
+        }
+
         $text = Markup::variant([
             'Выполняется звонок Б{{е},{+э}}ну...',
             'Звоню Б{{е},{+э}}ну, секунду...',
